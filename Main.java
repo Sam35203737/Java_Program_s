1407221780c8cffe7b40963ed8eea582a55cac5b
Sam35203737 <mrityunjaysamdarshi3737@gmail.com>
2024-07-15 01:54:28 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,5 +4,6 @@ public static void main(String[] args) {
     System.out.println("This is my output");
     System.out.println("Now i am ready to fire");
     System.out.println(" I am ready to fight");
+    System.out.println(new date());
 }
 }
