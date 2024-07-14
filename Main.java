ff27d5f5c1af244b5d540affb3d856e39cb6ea78
Sam35203737 <mrityunjaysamdarshi3737@gmail.com>
2024-07-15 01:57:53 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,11 @@
+import java.util.*;
 public class Main{
 public static void main(String[] args) {
     System.out.println("This is Mycode");
     System.out.println("This is my output");
     System.out.println("Now i am ready to fire");
     System.out.println(" I am ready to fight");
-    System.out.println(new date());
+    System.out.println("This is first Change");
+    System.out.println("This is Mrityunjsy");
 }
 }
